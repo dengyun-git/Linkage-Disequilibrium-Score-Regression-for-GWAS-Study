eecaafe53fc98de5546380a22eba1b78ea3de1a0
YUN <53471532+dengyun-git@users.noreply.github.com>
2024-04-03 14:08:55 +0100

    Update finalRun.sh

--- finalRun.sh
+++ finalRun.sh
@@ -2,10 +2,10 @@
 
 ### run R script subanalysis1.3.R. This R script perform the major functionality analysis, and also generate gene annotation sets for ldsc heritability enrichment analysis.
 
-Rscript /Users/ydeng/Documents/QCstepOA/DA_STEpUP_202111/DAcode2021.11/PrimaryAnalysis/subanalysis1.3.R
+Rscript /Users/ydeng/Documents/DAcode2021.11/PrimaryAnalysis/subanalysis1.3.R
 
 ### at the same time run bash to retreive refSNP ID for all the snp locations in GWAS file 
-#bash /Users/ydeng/Documents/QCstepOA/DA_STEpUP_202111/MetaIn1.1/tempOut4In/ldsc/getSNPoa.sh 
+#bash /Users/ydeng/Documents/MetaIn1.1/tempOut4In/ldsc/getSNPoa.sh 
 #wait
 
 
